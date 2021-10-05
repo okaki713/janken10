ed8d3d6723b06125a076f18867bed5a7cd2f2cae
okaki713 <e1b19014@oit.ac.jp>
2021-10-05 16:13:58 +0900

    エラー修正完了

--- src/main/java/oit/is/z1246/kaizi/janken10/controller/Sample21Controller.java
+++ src/main/java/oit/is/z1246/kaizi/janken10/controller/Sample21Controller.java
@@ -1,4 +1,4 @@
-package oit.is.inudaisuki.springboot_samples.controller;
+package oit.is.z1246.kaizi.janken10.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
