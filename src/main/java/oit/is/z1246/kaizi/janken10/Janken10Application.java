88f0ed62ddffc5ceb821652411af52c36a270883
okaki713 <e1b19014@oit.ac.jp>
2021-10-05 14:16:00 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/z1246/kaizi/janken10/Janken10Application.java
@@ -0,0 +1,13 @@
+package oit.is.z1246.kaizi.janken10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Janken10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Janken10Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/z1246/kaizi/janken10/Janken10ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z1246.kaizi.janken10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Janken10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
